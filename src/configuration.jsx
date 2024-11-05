19d486be31af3a457c42e2e022ca781c8178c966
Rizqy-617 <rizqyandriansyah2205@gmail.com>
2024-11-05 15:14:13 +0700

    add2

--- src/configuration.jsx
+++ src/configuration.jsx
@@ -17,7 +17,7 @@ const firebaseConfig = {
   measurementId: "G-F4LZ1R1KEK"
 };
 
-const TRACKING_ID = "G-F4LZ1R1KEK";
+const TRACKING_ID = "G-CYLMJ0XRLL";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
