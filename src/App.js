fe7598c415e7071254f89fd60a4e46de39991374
Rizqy-617 <rizqyandriansyah2205@gmail.com>
2024-11-06 14:17:00 +0700

    add classes check

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} id='image_logo' className="App-logo testing" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
